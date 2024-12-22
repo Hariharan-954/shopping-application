3e54bddb6a55c54abe160b708552bbc4ad5a7695
Hariharan-GS <hariharan.gs@unifo.in>
2024-12-22 20:59:24 +0530

    updated-baseName

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CardList from "./components/CardList";
 import Login from "./components/Login";
 import Cart from "./components/Cart";
 import ProductSpec from "./components/ProductSpec";
 import WishList from './components/WishList';
 import Frame from './components/Frame';
-import ProtectedRoute from './components/ProtectedRoute'
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <Frame>
-      <Routes>
-         <Route path="/"           element={<Login />} />
-        <Route path="/products"    element={ <ProtectedRoute> <CardList /> </ProtectedRoute> } />
-        <Route path="/login"       element={<Login />} />
-        <Route path="/cart"        element={<ProtectedRoute><Cart /> </ProtectedRoute> }/>
-        <Route path="/productSpec" element={ <ProtectedRoute><ProductSpec /> </ProtectedRoute>}/>
-        <Route path="/wishList"    element={<ProtectedRoute> <WishList /> </ProtectedRoute>}
-        />
-      </Routes>
-    </Frame>
+    <Router basename="/shopping-application">
+      <Frame>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/products" element={<ProtectedRoute><CardList /></ProtectedRoute>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+          <Route path="/productSpec" element={<ProtectedRoute><ProductSpec /></ProtectedRoute>} />
+          <Route path="/wishList" element={<ProtectedRoute><WishList /></ProtectedRoute>} />
+        </Routes>
+      </Frame>
+    </Router>
   );
 }
 
