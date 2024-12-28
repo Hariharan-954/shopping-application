b05a3805441bc900f1c5435c96453746d116d9b4
Hariharan-GS <hariharan.gs@unifo.in>
2024-12-28 21:33:50 +0530

    redirect-fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import CardList from "./components/CardList";
 import Login from "./components/Login";
 import Cart from "./components/Cart";
@@ -8,17 +9,29 @@ import Frame from './components/Frame';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const redirectTo = params.get('redirectTo');
+
+    if (redirectTo) {
+      navigate(redirectTo, { replace: true });
+    }
+  }, [location, navigate]);
+
   return (
-      <Frame>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/products" element={<ProtectedRoute><CardList /></ProtectedRoute>} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
-          <Route path="/productSpec" element={<ProtectedRoute><ProductSpec /></ProtectedRoute>} />
-          <Route path="/wishList" element={<ProtectedRoute><WishList /></ProtectedRoute>} />
-        </Routes>
-      </Frame>
+    <Frame>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/products" element={<ProtectedRoute><CardList /></ProtectedRoute>} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
+        <Route path="/productSpec" element={<ProtectedRoute><ProductSpec /></ProtectedRoute>} />
+        <Route path="/wishList" element={<ProtectedRoute><WishList /></ProtectedRoute>} />
+      </Routes>
+    </Frame>
   );
 }
 
